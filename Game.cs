979147618e9d93b0d90c982258ca8df0bf087062
Thomas Diggs <thomasdiggs@gmail.com>
2024-03-27 20:30:09 -0600

    final commit

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -19,12 +20,14 @@ namespace ShootingGallery
         MouseState mState;
         bool mReleased = true;
         int score = 0;
+        double timer = 10;
+        int crosshairsRadius = 25;
 
         public Game()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
         }
 
         // Runs once at the start of the game to initialize the game world such as setting up variables, etc.
@@ -56,13 +59,25 @@ namespace ShootingGallery
                 Exit();
 
             // TODO: Add your update logic here
+            // GameTime is a class that provides a snapshot of the game timing state. It provides the elapsed time since the last update, the total time the game has been running, etc.
+            if (timer > 0)
+            {
+                timer -= gameTime.ElapsedGameTime.TotalSeconds; // gameTime.ElapsedGameTime.TotalSeconds is the time elapsed since the last update in seconds.
+            }
+            if (timer < 0)
+            {
+                timer = 0;
+            }
             mState = Mouse.GetState(); // Get the current state of the mouse. States could be the position of the mouse, the state of the buttons, etc.
             if (mState.LeftButton == ButtonState.Pressed && mReleased)
             {
                 float mouseTagetDist = Vector2.Distance(targetPosition, mState.Position.ToVector2());
-                if (mouseTagetDist < targetRadius)
+                if (mouseTagetDist < targetRadius && timer > 0)
                 {
                     score++;
+                    Random rand = new Random();
+                    targetPosition.X = rand.Next(0 + 45, _graphics.PreferredBackBufferWidth - 45); // PreferredBackBufferWidth is the width of the window.
+                    targetPosition.Y = rand.Next(0 + 45, _graphics.PreferredBackBufferHeight - 45); // PreferredBackBufferHeight is the height of the window.
                 }
                 mReleased = false;
             }
@@ -83,8 +98,13 @@ namespace ShootingGallery
             _spriteBatch.Begin(); // The Begin method must be called before any calls to draw sprites.
             // We draw the background sprite first so that it appears behind the other sprites. This is the concept of drawing order.
             _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White); // Draw the background sprite at position (0,0) with no tint.
-            _spriteBatch.DrawString(gameFont, score.ToString(), new Vector2(100, 100), Color.White);// We choose White as the tint color because we want the sprite to be drawn as it is.
-            _spriteBatch.Draw(targetSprite, targetPosition, Color.White);
+            _spriteBatch.DrawString(gameFont, "Score: " + score.ToString(), new Vector2(5, 5), Color.White); // We choose White as the tint color because we want the sprite to be drawn as it is.
+            _spriteBatch.DrawString(gameFont, "Timer: " + Math.Ceiling(timer).ToString(), new Vector2(5, 45), Color.White);
+            if (timer > 0)
+            {
+                _spriteBatch.Draw(targetSprite, new Vector2(targetPosition.X - targetRadius, targetPosition.Y - targetRadius), Color.White);
+            }
+            _spriteBatch.Draw(crosshairsSprite, new Vector2(mState.X - crosshairsRadius, mState.Y - crosshairsRadius), Color.White);
             _spriteBatch.End(); // The End method must be called after all calls to draw sprites.
 
             base.Draw(gameTime);
